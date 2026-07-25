95c497840ec0d041509737e90d1540056f7eb42e
Solorzano,JuanJose <juan.jose.solorzano@mail.schaeffler.com>
2026-07-25 11:36:56 -0600

    Updated: lspconfig.lua - C language supported for got the definitions

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,25 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+vim.lsp.config("clangd", {
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--completion-style=detailed",
+    "--header-insertion=iwyu",
+    "--cross-file-rename",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+  root_markers = {
+    "compile_commands.json",
+    "compile_flags.txt",
+    ".clangd",
+    ".git",
+  },
+})
+
+
+local servers = { "html", "cssls", "clangd" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
